1ee8009709da07d1aa8c2bcdb23dd43accb1e573
Jelena Mikovic <jelena.mikovic189@gmail.com>
2025-11-26 23:17:33 +0100

    Added drafts page
    Added view of all authors drafts, for each one option to delete, publish or update. Added a form for adding a new term.

--- front/front/src/app/app.module.ts
+++ front/front/src/app/app.module.ts
@@ -9,7 +9,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatTableModule } from '@angular/material/table';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -49,6 +50,8 @@ import { DraftsComponent } from './drafts/drafts.component';
     MatIconModule,
     ReactiveFormsModule,
     MatTableModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [
     { provide: MAT_DIALOG_DATA, useValue: {} },
