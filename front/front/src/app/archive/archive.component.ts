b24ca054753cab77701de996d814d8ad39c96fed
Jelena Mikovic <jelena.mikovic189@gmail.com>
2025-11-26 22:37:03 +0100

    Added archive page
    Added view of all archived terms for authors.

--- front/front/src/app/archive/archive.component.ts
+++ front/front/src/app/archive/archive.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TermService, TermDTO } from '../term.service';
+import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-archive',
   standalone: false,
   templateUrl: './archive.component.html',
-  styleUrl: './archive.component.css'
+  styleUrls: ['./archive.component.css']
 })
-export class ArchiveComponent {
+export class ArchiveComponent implements OnInit {
+  terms: TermDTO[] = [];
+  filteredTerms: TermDTO[] = [];
+  searchControl = new FormControl('');
+  displayedColumns: string[] = ['name', 'definition']; 
 
+  constructor(private termService: TermService) { }
+
+  ngOnInit(): void {
+    this.loadArchivedTerms();
+
+    this.searchControl.valueChanges.pipe(
+      debounceTime(300)
+    ).subscribe(searchText => {
+      const query = (searchText ?? '').toLowerCase();
+      this.filteredTerms = this.terms.filter(term =>
+        term.name.toLowerCase().includes(query)
+      );
+    });
+  }
+
+  loadArchivedTerms(): void {
+    this.termService.getArchivedTerms().subscribe({
+      next: (data) => {
+        this.terms = data;
+        this.filteredTerms = data;
+      },
+      error: (err) => console.error(err)
+    });
+  }
 }
